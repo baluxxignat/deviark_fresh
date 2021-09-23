1fb60ddcde4c9b9d4e97cbc4f23e1b66e13389cc
baluxxignat <aleksandr.pravo@gmail.com>
2021-09-23 21:56:09 +0300

    first settings

---
+++ config/variables.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PORT: process.env.PORT || 5000,
+    DATA_BASE_URL: process.env.DATA_BASE_URL || 'mongodb+srv://root:root@cluster0.997tk.mongodb.net/deviark?retryWrites=true&w=majority',
+};
