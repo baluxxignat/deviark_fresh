f8e0609e759fcd65aefb2a2c3d97be56e0154c3c
Oleksandr Ignatov <aleksandr.pravo@gmail.com>
2021-09-23 22:04:31 +0300

    Delete variables.js

--- config/variables.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 5000,
-    DATA_BASE_URL: process.env.DATA_BASE_URL || 'mongodb+srv://root:root@cluster0.997tk.mongodb.net/deviark?retryWrites=true&w=majority',
-};
